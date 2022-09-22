41be6752af93f74863bb2b2ec04c4d1434e39ea1
Paul Thimote <pthimote@gmail.com>
2022-09-21 22:45:27 -0400

    commit tree 338d7dfd6a73dfefa96322c1a7d0e1f976d8d817

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('text').textContent = 'This is really cool!'
+  });
+
+// function happy() {
+//     let p = document.createElement('p')
+//     p.textContent = 'This is really cool!'
+// }
+//   document.getElementById('p').textContent = 'This is really cool!'
+
+//   window.addEventListener('DOMContentLoaded', (event) => {
+//     console.log('DOM fully loaded and parsed');
+// });
